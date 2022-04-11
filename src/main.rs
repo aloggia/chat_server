fa3c35b0864efa80648e5303730d3af58d0bf316
aloggia <aloggia22@gmail.com>
2022-04-11 15:02:30 -0400

    Skeleton code that will accept incoming TCP connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use async_std::{prelude::*,
+task,
+net::{TcpListener, ToSocketAddrs}};
+use std::option::Option::Some;
+use futures::StreamExt;
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener: std::net::TcpListener = unimplemented!();
+    for stream in listener.incoming() {
+        //TODO
+    }
+}
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    let mut incoming = listener.incoming();
+    while let Some(stream) = incoming.next().await {
+        //TODO
+    }
+    Ok(())
 }
