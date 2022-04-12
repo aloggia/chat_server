2bd3f2d059533cfb6633cc7fa145f8358d84766d
aloggia <aloggia22@gmail.com>
2022-04-12 14:03:01 -0400

    Created an enum to handle new users and messages, added a Sender and Receiver type, added a writer loop to write any incoming messages to their designated sockets, added funtion to log errors, added a function to hold a hashmap of all users, without the need for a writer lock

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
+#![allow(unused)]
 use async_std::{prelude::*,
 task,
 net::{TcpListener, ToSocketAddrs, TcpStream},
 io::BufReader,
 };
 use std::option::Option::Some;
-use futures::{StreamExt, AsyncBufReadExt};
+use futures::{StreamExt, AsyncBufReadExt, AsyncWriteExt};
+use futures::sink::SinkExt;
+use futures::channel::mpsc;
+use std::sync::Arc;
+use std::collections::hash_map::{Entry, HashMap};
+
+#[derive(Debug)]
+enum Event {
+    NewPeer {
+        name: String,
+        stream: Arc<TcpStream>
+    },
+    Message {
+        source: String,
+        // dest is a vec of strings, because users can send messages to multiple people
+        dest: Vec<String>,
+        msg: String,
+    }
+}
 
 
 fn main() {
@@ -12,6 +31,8 @@ fn main() {
 }
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type Sender<T> = mpsc::UnboundedSender<T>;
+type Receiver<T> = mpsc::UnboundedReceiver<T>;
 
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
@@ -54,8 +75,65 @@ async fn connection_loop(tcp_stream: TcpStream) -> Result<()> {
 
 }
 
+async fn connection_writer_loop(mut messages: Receiver<String>, stream: Arc<TcpStream>)
+-> Result<()> {
+    let mut stream = &*stream;
+    while let Some(msg) = messages.next().await {
+        stream.write_all(msg.as_bytes()).await?;
+    }
+    Ok(())
+}
+
 fn run() -> Result<()> {
     // Put the Servers IP address here, idk what it is yet
     let fut = accept_loop();
     task::block_on(fut)
+}
+
+fn spawn_log_errors<F>(fut: F) -> task::JoinHandle<()>
+    where
+        F: Future<Output=Result<()>> + Send + 'static,
+{
+    task::spawn(async move {
+        if let Err(e) = fut.await {
+            eprintln ! ("{}", e)
+        }
+    })
+}
+
+async fn broker_loop(mut events: Receiver<Event>) -> Result<()> {
+    // Hash map that maps users to destination channels
+    let mut peers: HashMap<String, Sender<String>> = HashMap::new();
+
+    while let Some(event) = events.next().await {
+        match event {
+            // Message event, server recieved a message from input, this forwards it to the correct client
+            Event::Message { source, dest, msg} => {
+                for addr in dest {
+                    if let Some(peer) = peers.get_mut(&addr) {
+                        // Properly formatted in UVMPM protocol sending style
+                        let msg = format!("From:{}:{}\n", source, msg);
+                        peer.send(msg).await?
+                    }
+                }
+            }
+            // New user has joined the server, we need to add them to the hash map of clients connected
+            // to the server
+            Event::NewPeer { name, stream} => {
+                match peers.entry(name) {
+                    // user is already in the hash map
+                    Entry::Occupied(..) => (),
+                    Entry::Vacant(entry) => {
+                        let (client_sender, client_receiver) = mpsc::unbounded();
+                        entry.insert(client_sender);
+                        // This task will write any messages to the new peers socket
+                        spawn_log_errors(connection_writer_loop(client_receiver, stream));
+                    }
+                }
+            }
+        }
+    }
+
+
+    Ok(())
 }
